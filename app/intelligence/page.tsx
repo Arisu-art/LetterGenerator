2086e34c1e50f2d92d191e90144eb02c5dfb29ad
Arisu-art <83152637+Arisu-art@users.noreply.github.com>
2026-06-01 12:23:28 +0800

    Expose intelligent workspace experience

---
+++ app/intelligence/page.tsx
@@ -0,0 +1,11 @@
+import LetterGeneratorWorkspace from '../../components/LetterGeneratorWorkspace';
+import AdaptiveCommandCenter from '../../components/AdaptiveCommandCenter';
+
+export default function IntelligentWorkspacePage() {
+  return (
+    <>
+      <LetterGeneratorWorkspace />
+      <AdaptiveCommandCenter />
+    </>
+  );
+}
