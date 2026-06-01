c78cc35f677e6fd81b6ce17ef4d09b58deaf6467
Arisu-art <83152637+Arisu-art@users.noreply.github.com>
2026-06-01 13:23:23 +0800

    Enable ordered packet workflow with blank reserved positions

--- components/IntelligentWorkspaceView.tsx
+++ components/IntelligentWorkspaceView.tsx
@@ -1,13 +1,13 @@
 'use client';
 
-import LetterGeneratorWorkspace from './LetterGeneratorWorkspace';
+import LetterGeneratorWorkspaceV2 from './LetterGeneratorWorkspaceV2';
 import PerformanceBeacon from './PerformanceBeacon';
 import PacketMapPreviewController from './PacketMapPreviewController';
 
 export default function IntelligentWorkspaceView() {
   return (
     <>
-      <LetterGeneratorWorkspace />
+      <LetterGeneratorWorkspaceV2 />
       <PerformanceBeacon />
       <PacketMapPreviewController />
     </>
