a9cfcfc0f271c3247746f0396c2781716c3e8f40
Arisu-art <83152637+Arisu-art@users.noreply.github.com>
2026-06-01 12:49:33 +0800

    Remove adaptive command center from workspace

--- app/intelligence/page.tsx
+++ app/intelligence/page.tsx
@@ -1,13 +1,11 @@
 import styles from './intelligence.module.css';
 import LetterGeneratorWorkspace from '../../components/LetterGeneratorWorkspace';
-import AdaptiveCommandCenter from '../../components/AdaptiveCommandCenter';
 import PerformanceBeacon from '../../components/PerformanceBeacon';
 
 export default function IntelligentWorkspacePage() {
   return (
     <div className={styles.experience}>
       <LetterGeneratorWorkspace />
-      <AdaptiveCommandCenter />
       <PerformanceBeacon />
     </div>
   );
