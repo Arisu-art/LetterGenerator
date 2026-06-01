135b35297e0b7a140656b071880b067a413f5a0a
Arisu-art <83152637+Arisu-art@users.noreply.github.com>
2026-06-01 19:03:10 +0800

    feat: make packet preview explicitly user initiated

--- components/IntelligentWorkspaceView.tsx
+++ components/IntelligentWorkspaceView.tsx
@@ -2,15 +2,17 @@
 
 import LetterGeneratorWorkspaceV2 from './LetterGeneratorWorkspaceV2';
 import PerformanceBeacon from './PerformanceBeacon';
-import AutomaticPacketPreview from './AutomaticPacketPreview';
 import ContinuousPacketUpgrade from './ContinuousPacketUpgrade';
 
+/**
+ * The intelligence surface remains lightweight until a user intentionally enters
+ * a document preview. Heavy packet rendering is no longer triggered on modal open.
+ */
 export default function IntelligentWorkspaceView() {
   return (
     <>
       <LetterGeneratorWorkspaceV2 />
       <PerformanceBeacon />
-      <AutomaticPacketPreview />
       <ContinuousPacketUpgrade />
     </>
   );
