c613c1c982ff72e7ab4024e421d2d8b482d0c8b2
Arisu-art <83152637+Arisu-art@users.noreply.github.com>
2026-06-01 12:31:14 +0800

    Activate adaptive experience styling

--- app/intelligence/page.tsx
+++ app/intelligence/page.tsx
@@ -1,11 +1,12 @@
+import styles from './intelligence.module.css';
 import LetterGeneratorWorkspace from '../../components/LetterGeneratorWorkspace';
 import AdaptiveCommandCenter from '../../components/AdaptiveCommandCenter';
 
 export default function IntelligentWorkspacePage() {
   return (
-    <>
+    <div className={styles.experience}>
       <LetterGeneratorWorkspace />
       <AdaptiveCommandCenter />
-    </>
+    </div>
   );
 }
