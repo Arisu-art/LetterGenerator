b5f925c9c4b5b1694e1280f3dc0a709437113296
Arisu-art <83152637+Arisu-art@users.noreply.github.com>
2026-06-04 13:22:54 +0800

    feat: surface document job activity in workspace

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import LetterGeneratorWorkspaceV2 from '../components/LetterGeneratorWorkspaceV2';
+import GenerationActivityMonitor from '../components/GenerationActivityMonitor';
 
 export default function Page() {
-  return <LetterGeneratorWorkspaceV2 />;
+  return <><LetterGeneratorWorkspaceV2 /><GenerationActivityMonitor /></>;
 }
